595a9dd0b22087aa35fca57687b1678ceabc0689
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-01-30 22:03:14 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 rpm-ostree install rclone
+rpm-ostree install vim
 
 # Use a COPR Example:
 #
