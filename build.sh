a2b26295199432653614bf057cc58fa2988a7e0c
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-01-31 13:25:49 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,25 +2,19 @@
 
 set -ouex pipefail
 
-### Install packages
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# --- Copy Custom Files & Overides ---
+
+# Change the autologin from gamescope-session
+# cp  /etc/sddm.conf.d/steamos.conf
+
+
+# --- Install Fedora Fackages ---
+
 rpm-ostree install rclone
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+# --- Enable System Unit Files ---
 
 systemctl enable podman.socket
 systemctl enable tailscaled
+systemctl enable ublue-update.timer
 
-#### Remove the autologin to gamescope-session
-# rm /etc/sddm.conf.d/steamos.conf
-#### Undo some bazzite-deck specific services we do not need anymore
-# systemctl disable bazzite-autologin.service && \
-# systemctl enable ublue-update.timer
