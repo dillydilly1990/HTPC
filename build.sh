3e1c48ce2173fdc75b6b47057816a0f3eae05398
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-01-30 21:33:50 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,21 +2,11 @@
 
 set -ouex pipefail
 
-### Install repos
-rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 ### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf install -y tmux 
-dnf install -y rclone
-dnf install -y kodi
-
+rpm-ostree install rclone
+rpm-ostree install emudeck
 
 # Use a COPR Example:
 #
