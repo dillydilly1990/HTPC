8a32aa56db21b89b31c0276f82bd457bcbaa84b3
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-02-02 21:24:11 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,27 +4,19 @@ set -ouex pipefail
 
 # --- Copy Custom Files & Overides ---
 
-
 # --- Install Emudeck ---
 #curl -L https://raw.githubusercontent.com/dragoonDorise/EmuDeck/main/install.sh | bash
 
-# --- Install Kodi flatpack
-mkdir -p /tmp/build/kodi/
-cd /tmp/build/kodi/
-git clone https://github.com/flathub/tv.kodi.Kodi.git
-flatpak-builder build-dir --user --ccache --force-clean --install tv.kodi.Kodi.yml
-
-#flatpak install -y --noninteractive flathub tv.kodi.Kodi
-
+# --- Install Kodi
+flatpak install -y --noninteractive flathub tv.kodi.Kodi
 
 # Change the autologin from gamescope-session
 # cp  /etc/sddm.conf.d/steamos.conf
-# --- Install Fedora Fackages ---
 
+# --- Install Rclone ---
 rpm-ostree install rclone
 
 # --- Enable System Unit Files ---
-
 systemctl enable podman.socket
 systemctl enable tailscaled
 systemctl enable ublue-update.timer
