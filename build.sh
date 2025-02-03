5b2ae2d12ff8d3d79cbf129cd612d7a845b23ae5
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-02-02 21:02:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,13 @@ curl -L https://raw.githubusercontent.com/dragoonDorise/EmuDeck/main/install.sh 
 # Change the autologin from gamescope-session
 # cp  /etc/sddm.conf.d/steamos.conf
 
-# --- Install Flatpacks
-flatpak install -y --noninteractive flathub tv.kodi.Kodi
+# --- Install Kodi flatpack
+mkdir /tmp/build/kodi/
+cd /tmp/build/kodi/
+git clone https://github.com/flathub/tv.kodi.Kodi.git
+flatpak-builder build-dir --user --ccache --force-clean --install tv.kodi.Kodi.yml
+
+#flatpak install -y --noninteractive flathub tv.kodi.Kodi
 # --- Install Fedora Fackages ---
 
 rpm-ostree install rclone
