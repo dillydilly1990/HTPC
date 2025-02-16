edfb935b66353d5108e3c2d8f244c8ecd82f2bc2
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-02-16 17:36:17 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-
 set -ouex pipefail
 
-# --- Copy Custom Files & Overides ---
-
-# --- Install Emudeck ---
-#curl -L https://raw.githubusercontent.com/dragoonDorise/EmuDeck/main/install.sh | bash
-
-# --- Install skip-1s app
-
-
-# Change the autologin from gamescope-session
-# cp  /etc/sddm.conf.d/steamos.conf
-
-# --- Install Rclone ---
 rpm-ostree install rclone
 rpm-ostree install kodi
 
-# --- Enable System Unit Files ---
+# --- systemd ---
 systemctl enable podman.socket
 systemctl enable tailscaled
 systemctl enable ublue-update.timer
