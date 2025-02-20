00eb6f2f1de19523b422e0cc672b3cb06f7f7bdc
dillydilly1990 <69658010+dillydilly1990@users.noreply.github.com>
2025-02-20 09:56:08 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,20 @@ set -ouex pipefail
 rpm-ostree install rclone
 rpm-ostree install kodi
 
+echo 'install build dependencys'
+rpm-ostree install SDL2-devel SDL2_image-devel SDL2_ttf-devel inih-devel
+echo 'clone master repo and create build dir'
+git clone https://github.com/complexlogic/flex-launcher.git
+cd flex-launcher
+mkdir build && cd build
+echo 'generate make file'
+cmake .. -DCMAKE_BUILD_TYPE=Release
+echo 'build and test'
+make
+./flex-launcher
+echo 'install'
+sudo make install
+
 # --- systemd ---
 systemctl enable podman.socket
 systemctl enable tailscaled
